d2c419869a6a69c5995bc1577e0708d83db2d68a
Fabricio-Barreto <63824002+Fabricio-Barreto@users.noreply.github.com>
2022-03-16 23:23:07 -0300

    Add Port process.env.PORT

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const connectToDb = require('./database/db')
 
 connectToDb();
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000
 
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")))
